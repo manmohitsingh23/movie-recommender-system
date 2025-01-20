d5db0be973e29303d76b8f9bdd5c10e5923e9dcf
Manmohit <manmohitsingh104@gmail.com>
2025-01-21 05:14:11 +0530

    Download similarity.pkl from Google Drive

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import pandas as pd
 
 def fetch_poster(movie_id):
+    # Function to fetch movie posters
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     data = requests.get(url)
     data = data.json()
@@ -12,6 +13,7 @@ def fetch_poster(movie_id):
     return full_path
 
 def recommend(movie):
+    # Function to recommend movies based on similarity
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
@@ -22,29 +24,38 @@ def recommend(movie):
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
-    return recommended_movie_names,recommended_movie_posters
+    return recommended_movie_names, recommended_movie_posters
 
 
 st.header('Movie Recommender System')
-movies_dict = pickle.load(open('./movie_dict.pkl','rb'))
+
+# Load movie data from pickle file
+movies_dict = pickle.load(open('./movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
-url = "https://drive.google.com/uc?id=1aAMB06nAttccKRRaAGTHK76uKLzSiCPz"  # Replace with your actual link
+# Download the similarity.pkl file from Google Drive
+url = "https://drive.google.com/uc?id=1aAMB06nAttccKRRaAGTHK76uKLzSiCPz"  # Replace with your actual Google Drive link
 r = requests.get(url)
 
+# Save the downloaded file locally
 with open("similarity.pkl", 'wb') as f:
     f.write(r.content)
-    
-similarity = pickle.load(open('similarity.pkl', 'rb'))    
 
+# Load the similarity matrix from the downloaded file
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# Dropdown for movie selection
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
     "Type or select a movie from the dropdown",
     movie_list
 )
 
+# Show recommendations on button click
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    
+    # Display recommendations in columns
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_movie_names[0])
@@ -52,7 +63,6 @@ if st.button('Show Recommendation'):
     with col2:
         st.text(recommended_movie_names[1])
         st.image(recommended_movie_posters[1])
-
     with col3:
         st.text(recommended_movie_names[2])
         st.image(recommended_movie_posters[2])
@@ -62,7 +72,3 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
-
-
-
-
